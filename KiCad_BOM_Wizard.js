c53aba9e50a5a4576bd615cbf8d3e61ce44229d0
HashDefineElectronics <HashDefineElectronics@users.noreply.github.com>
2016-02-05 09:41:58 +0000

    - removed the plugin instruction from the KiCad_BOM_Wizard.js - Added the GNU general public license V3 to code and package.json - General update to package.json - update plugin revision

--- KiCad_BOM_Wizard.js
+++ KiCad_BOM_Wizard.js
@@ -1,121 +1,36 @@
-/*
-@package
-# KiCad_BOM_Wizard
-
-### Author:
-Ronald Sousa http://hashdefineelectronics.com/kicad-bom-wizard/
-
-### Revision:
-0
-
-### Repository:
-https://github.com/HashDefineElectronics/KiCad_BOM_Wizard.git
-
-### Project Page:
-http://hashdefineelectronics.com/kicad-bom-wizard/
-
-# Description:
-This is the repository for KiCad_BOM_Wizard. This KiCad plugin can be used to create custom BOM files based on easy configurable templates files. The plugin is writing in JavaScript and has been designed to integrate into KiCad’s BOM plugin manager.
-
-The Idea for this plugin came from our need to generate BOM that are specific to of our clients needs. for example, some of our clients require their product to have document traceability due to their product ATEX certificate requirement.
-With KiCad_BOM_Wizard, We simply made a template that the output file includes the document number, project revision, and manufacture notes.
-
-By default, KiCad_BOM_Wizard comes with two templates, one will generate a stand along HTML file and the other will generate a CSV file.
-They are both include to simplify the use of plugin and can be used as an example by those who want to make their own templates. The latter could be due to needing to have your own company or project logo.
-
-KiCad_BOM_Wizard works by scanning through all of the template files and replacing any of the Short Codes with the data that is associated with it. It will then output all of the data it has collected including the file structure
-into one file based on the order that it finds the short codes.
-
-For example, if the KiCad_BOM_Wizard finds the short code <!--TAG_TITLE--> in template.conf then it we replace it with the KiCad project root sheet title. KiCad_BOM_Wizard will also group and sort all components together that have same parts value, the same starting designator reference prefix and the same fields value.
-
-For example, if your project component list consist of;
-> R1 10K, R2 100K, C1 10pF and R3 10K
-
-then it would be grouped like this;
-
-> | Ref | qty | value |
-> |----|-----|-----|
-> |C1 | 1 | 10pF |
-> | R1 R3 | 2 | 10K|
-> | R2| 1 | 100K|
-
-## For more details on how to use and make your own template that KiCad_BOM_Wizard can use, please visit the main project page. http://hashdefineelectronics.com/kicad-bom-wizard/
-
-# The following serves as quick reference.
-
-## installing nodejs in Linux:
-```sh
-sudo apt-get install nodejs
-sudo apt-get install npm
-```
-## installing nodejs on other system:
-    https://nodejs.org/en/download/
-
-## Kicad BOM Plugin Manager Command Line:
-#For HTML BOM
-    node "SCRIPT_ROOT_DIR/KiCad_BOM_Wizard.js" "%I" "%O.html"
-    // This is the same as
-    node "SCRIPT_ROOT_DIR/KiCad_BOM_Wizard.js" "%I" "%O.html" "HTML"
-    // This is the same as
-    node "SCRIPT_ROOT_DIR/KiCad_BOM_Wizard.js" "%I" "%O.html" "SCRIPT_ROOT_DIR/Template/HTML"
-
-#For CSV BOM
-    node "SCRIPT_ROOT_DIR/KiCad_BOM_Wizard.js" "%I" "%O.csv" "CSV"
-    // This is the same as
-    node "SCRIPT_ROOT_DIR/KiCad_BOM_Wizard.js" "%I" "%O.csv" "SCRIPT_ROOT_DIR/Template/CSV"
-
-#Using your own template BOM
-    node "SCRIPT_ROOT_DIR/KiCad_BOM_Wizard.js" "%I" "%O.txt" "Path_To_Your_Template_conf/Your_Template"
-    // or if you are using the plugin template directory to store your template. "SCRIPT_ROOT_DIR/Template/"
-    node "SCRIPT_ROOT_DIR/KiCad_BOM_Wizard.js" "%I" "%O.txt" "Your_Template"
-
-where "%I" in the input kicad xml file and "%O" is the output directory and name for the BOM. This must include your file extension
-
-## short_codes list used by the template files
-
-### for template.conf:
-    <!--TITLE-->                        inserts the root sheet title.
-    <!--DATE-->                         inserts the root sheet date.
-    <!--DATE_GENERATED-->               inserts the date and time the Kicad net file was created
-    <!--COMPANY-->                      inserts the root sheet company name
-    <!--REVISON-->                      inserts the root sheet revision value
-    <!--COMMENT_1-->                    inserts the root sheet comment 1
-    <!--COMMENT_2-->                    inserts the root sheet comment 2
-    <!--COMMENT_3-->                    inserts the root sheet comment 3
-    <!--COMMENT_4-->                    inserts the root sheet comment 4
-    <!--TOTAL_NUM_OF_PARTS-->           inserts the number of parts used in the design
-    <!--TOTAL_NUM_OF_UNIQUE_PARTS-->    inserts the number of unique parts used in the design. Note, if two similar parts have different fields then it will be register as unique
-    <!--CLASS_HEADER_TAG-->       inserts the table headers
-    <!--BOM_TABLE-->                    inserts the complete generated BOM table
-
-### for headers.conf:
-    <!--HEADER_ROW-->         inserts the column title
-    <!--HEADER_CLASS_REF_TAG-->        insert the tag for the part reference. HeadRefTag
-    <!--HEADER_CLASS_QTY_TAG-->        insert the tag for the part qty. HeadQtyTag
-    <!--HEADER_CLASS_VALUE_TAG-->        insert the tag for the part value. HeadValueTag
-
-### for group.conf:
-    <!--GROUP_ROW_DATA-->       inserts the group of parts row data
-    <!--GROUP_CLASS_TAG-->    inserts the group class name. format "group_" + "part ref prefix"
-    <!--GROUP_TITLE_TEXT-->    inserts the group title. the part ref prefix
-
-### for row.conf:
-    <!--ROW_PART_REF-->            inserts the list of parts reference designator
-    <!--ROW_PART_QTY-->            inserts the number of parts grouped together
-    <!--ROW_PART_VALUE-->          inserts the part value
-    <!--ROW_PART_FIELDS-->          inserts the generator parts fields
-    <!--ROW_CLASS_ODD_EVEN_TAG-->    returns RowEvenTag on even rows or RowOddTag for odds rows.
-    <!--HEADER_CLASS_REF_TAG-->         insert the tag for the part reference. HeadRefTag
-    <!--HEADER_CLASS_QTY_TAG-->         insert the tag for the part qty. HeadQtyTag
-    <!--HEADER_CLASS_VALUE_TAG-->         insert the tag for the part value. HeadValueTag
-
-### for fields.conf:
-    <!--FIELD_CLASS_TAG-->    inserts the fields class name
-    <!--FIELD-->              inserts the field value
-*/
+/////////////////////////////////////////////////////////////////
+/// This is the source code file KiCad_BOM_Wizard.js, this is a free KiCad BOM plugin.
+/// Copyright (C) 2016  Ronald A. N. Sousa www.hashdefineelectronics.com/
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///    
+/// \file KiCad_BOM_Wizard.js
+/// \Author Ronald Sousa http://hashdefineelectronics.com/kicad-bom-wizard/
+/// \revision 0.1
+/// 
+/// \repository: https://github.com/HashDefineElectronics/KiCad_BOM_Wizard.git
+/// 
+/// \website http://hashdefineelectronics.com/kicad-bom-wizard/
+/// 
+/// \brief This KiCad plugin can be used to create custom BOM files based on easy 
+/// configurable templates files. The plugin is writing in JavaScript and has been 
+/// designed to integrate into KiCad’s BOM plugin manager.
+/// 
+/////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////
-/// \brief defines the plugin revison number
+/// \brief defines the plugin revision number
 /////////////////////////////////////////////////////////////////
 var PluginRevisionNumber = "0";
 
@@ -363,8 +278,6 @@ function GenerateTable(fieldsList, groupedList, partGroupedList)
     return ReturnOutput;
 }
 
-
-
 /////////////////////////////////////////////////////////////////
 /// \brief return the generated part table
 ///
@@ -542,7 +455,7 @@ function ReadXmlFile()
 
                     if (UserProjectNetData.export.$.version != KiCadXMLRevision)
                     {
-                        ErrorMessage("Incompatible KiCad XML version: Expexted " + KiCadXMLRevision + " Found " + UserProjectNetData.export.$.version);
+                        ErrorMessage("Incompatible KiCad XML version: Expected " + KiCadXMLRevision + " Found " + UserProjectNetData.export.$.version);
                     }
 
                     Task("STATE_READ_TEMPLATE");
